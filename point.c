3ea2cec5bc9acf3e434c82fe6a34220bb111bcb0
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-04-13 01:48:31 +0530

    Added a file point.c with get_closest_food function.

---
+++ point.c
@@ -0,0 +1,22 @@
+#include "point.h"
+#include <stdio.h>
+
+void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
+{
+  int current_distance;
+  int previous_distance = __INT_MAX__;
+  for (int count = 0; count < points_length; count++)
+  {
+    int difference_of_x_cords = current_location.x - food_points[count].x;
+    int difference_of_y_cords = current_location.y - food_points[count].y;
+    int square_of_difference_of_x = difference_of_x_cords * difference_of_x_cords;
+    int square_of_difference_of_y = difference_of_y_cords * difference_of_y_cords;
+    int current_distance = square_of_difference_of_x + square_of_difference_of_y;
+    if (current_distance < previous_distance)
+    {
+      previous_distance = current_distance;
+      closest_food_location->x = food_points[count].x;
+      closest_food_location->y = food_points[count].y;
+    }
+  }
+}
