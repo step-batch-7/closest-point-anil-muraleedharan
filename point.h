0d7857adf16580f0f7fc04399bd911b781b74904
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-04-13 14:26:45 +0530

    Moved struct declaration & typedef into same line

--- point.h
+++ point.h
@@ -1,9 +1,7 @@
-struct Point
+typedef struct
 {
   int x;
   int y;
-} ;
-
-typedef struct Point POINT;
+} POINT;
 
 void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location);
