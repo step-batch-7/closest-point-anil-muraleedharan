9cad201cf028676e8e5630a8c7fd53fad5f76ceb
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-04-13 14:23:17 +0530

    Made a function find distance of two points.

--- point.c
+++ point.c
@@ -1,16 +1,20 @@
 #include "point.h"
+#include <math.h>
+
+double distance_of_two_points (POINT first_point, POINT second_point)
+{
+    double square_of_difference_of_x = pow(first_point.x - second_point.x,2);
+    double square_of_difference_of_y = pow(first_point.y - second_point.y,2);
+    return sqrt(square_of_difference_of_x + square_of_difference_of_y);
+}
 
 void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location)
 {
-  int current_distance;
-  int previous_distance = __INT_MAX__;
+  double current_distance;
+  double previous_distance = __INT_MAX__;
   for (int count = 0; count < points_length; count++)
   {
-    int difference_of_x_cords = current_location.x - food_points[count].x;
-    int difference_of_y_cords = current_location.y - food_points[count].y;
-    int square_of_difference_of_x = difference_of_x_cords * difference_of_x_cords;
-    int square_of_difference_of_y = difference_of_y_cords * difference_of_y_cords;
-    int current_distance = square_of_difference_of_x + square_of_difference_of_y;
+    current_distance = distance_of_two_points(current_location, food_points[count]);
     if (current_distance < previous_distance)
     {
       previous_distance = current_distance;
